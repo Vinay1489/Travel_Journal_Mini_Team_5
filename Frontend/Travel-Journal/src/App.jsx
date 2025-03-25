a41cb1e3c33336fe131997ddabaac25ce8e5b1bd
Vinay <vinayvennampally@gmail.com>
2025-03-25 14:30:33 +0530

    Updated App.jsx

--- Frontend/Travel-Journal/src/App.jsx
+++ Frontend/Travel-Journal/src/App.jsx
@@ -1,30 +1,26 @@
-import TravelJournal from "./components/HomePage/TravelJournal"
-import NewEntry from "./components/EntryPage/NewEntry"
-import JourneyDetails from "./components/JourneyDetail/JourneyDetails"
-import JourneyMap from "./components/JourneysMap/JourneyMap"
-import PhotoGallery from "./components/GalleryPage/PhotoGallery"
-import ProfilePage from "./components/ProfilePage/ProfilePage"
-import SettingsPage from "./components/AccountSettingsPage/SettingsPage"
-import LoginPage from "./components/LoginPage/LoginPage"
-import SignupPage from "./components/SignUpPage/CreateAccountPage"
+import TravelJournal from "./components/HomePage/TravelJournal";
+import NewEntry from "./components/EntryPage/NewEntry";
+import JourneyDetails from "./components/JourneyDetail/JourneyDetails";
+import JourneyMap from "./components/JourneysMap/JourneyMap";
+import PhotoGallery from "./components/GalleryPage/PhotoGallery";
+import ProfilePage from "./components/ProfilePage/ProfilePage";
+import SettingsPage from "./components/AccountSettingsPage/SettingsPage";
+import LoginPage from "./components/LoginPage/LoginPage";
+import SignupPage from "./components/SignUpPage/CreateAccountPage";
 
-
-export default function App()
-{
-  return(
+export default function App() {
+  return (
     <div>
-    <TravelJournal></TravelJournal>
-    <NewEntry/>
-    <JourneyDetails/>
-    <JourneyMap/>
-    <PhotoGallery/>
-    <ProfilePage/>
-    <SettingsPage/>
-    <LoginPage/>
-    <SignupPage/>
-  
-  
+      <TravelJournal></TravelJournal>
+      <NewEntry />
+      <JourneyDetails />
+      <JourneyMap />
+      <PhotoGallery />
+      <ProfilePage />
+      <SettingsPage />
+      <LoginPage />
+      <SignupPage />
+      <SignupPage></SignupPage>
     </div>
-
-  )
+  );
 }
