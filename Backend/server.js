dd6a3602d42bfd9c5533c0d0a1acb92b07369ca8
Sai Vinay <167610185+Vinay1489@users.noreply.github.com>
2025-06-11 06:50:14 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -21,11 +21,6 @@ mongoose
     console.log("Error in connecting " + err);
   });
 
-app.get("/", (req, res) => {
-  res
-    .status(200)
-    .json({ message: "Hello from the server side!", app: "Travel-Journal" });
-});
 
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
