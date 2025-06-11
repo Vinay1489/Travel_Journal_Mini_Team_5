ad5902bc0dde678fd91ac3bcb29861baa24fbfff
Sai Vinay <167610185+Vinay1489@users.noreply.github.com>
2025-06-11 07:14:07 +0530

    Update reviews.js

--- Frontend/Travel-Journal/src/api/reviews.js
+++ Frontend/Travel-Journal/src/api/reviews.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const REVIEW_API = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: "https://travel-journal-w8vd.onrender.com/api/v1",
   withCredentials: true,
 });
 
