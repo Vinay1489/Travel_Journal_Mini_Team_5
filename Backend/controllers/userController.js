153c09cd0b8161451091e2d55949810a02799ac0
Sai Vinay <167610185+Vinay1489@users.noreply.github.com>
2025-07-13 05:23:20 +0530

    Update userController.js

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -32,7 +32,7 @@ exports.resizeUserPhoto = async (req, res, next) => {
     .resize(500, 500)
     .toFormat('jpeg')
     .jpeg({ quality: 90 })
-    .toFile(`public/img/users/${req.file.filename}`);
+    .toFile(`uploads/users/${req.file.filename}`);
 
   next();
 };
